cb607c7da6d3313966d7488f01b3dd404a7d4ffd
Max Quiet <qbite0@gmail.com>
2023-03-14 22:48:21 +0700

    updatik

--- GeneralEmployeeTerminal/ViewModels/AuthViewModel.cs
+++ GeneralEmployeeTerminal/ViewModels/AuthViewModel.cs
@@ -3,30 +3,45 @@ using System.Threading.Tasks;
 using System;
 using Avalonia.Controls;
 using ReactiveUI;
+using MessageBox.Avalonia;
+using GeneralEmployeeTerminal.Views;
 
-namespace GeneralEmployeeTerminal.ViewModels {
-    public class AuthViewModel : ViewModelBase {
+namespace GeneralEmployeeTerminal.ViewModels
+{
+    public class AuthViewModel : ViewModelBase
+    {
         const string API_URL = "https://localhost:7287";
 
-        private async Task<bool> IsEmployeeExistsAsync(Guid id) {
-            using (var client = new HttpClient()) {
+        private async Task<bool> IsEmployeeExistsAsync(Guid id)
+        {
+            using (var client = new HttpClient())
+            {
                 var response = await client.GetAsync($"{API_URL}/api/Employee/{id}");
                 return response.IsSuccessStatusCode;
             }
         }
 
         private string _searchTerm;
-        public string SearchTerm {
+        public string SearchTerm
+        {
             get => _searchTerm;
             set => this.RaiseAndSetIfChanged(ref _searchTerm, value);
         }
 
-        public async void ButtonClickedAsync() {
+        public async void ButtonClickedAsync()
+        {
             var result = await IsEmployeeExistsAsync(Guid.Parse(SearchTerm));
-            if (result == true) {
-                throw new Exception("Успешно");
-            } else {
-                throw new Exception("Произошла ошибка");
+            if (result == true)
+            {
+                var model = new MainWindow();
+                model.Show();
+            }
+            else
+            {
+                _ = MessageBoxManager.GetMessageBoxStandardWindow(
+                    "Ошибка",
+                    $"Произошла ошибка"
+                    ).Show();
             }
         }
     }
