e966fb02f5ae23d792003525ce6dbaeaa2bf3614
Max Quiet <qbite0@gmail.com>
2023-03-23 14:50:42 +0700

    da

--- GeneralEmployeeTerminal/Views/MainWindow.axaml.cs
+++ GeneralEmployeeTerminal/Views/MainWindow.axaml.cs
@@ -1,9 +1,23 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using GeneralEmployeeTerminal.Models;
+using MessageBox.Avalonia;
+using System;
 
 namespace GeneralEmployeeTerminal.Views {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
+            datagrid1.IsReadOnly = true;
+            datagrid1.DoubleTapped += Handler;
+        }
+
+        private void Handler(object? sender, RoutedEventArgs e)
+        {
+            Ticket ticket = (Ticket)datagrid1.SelectedItem;
+            var infoWin = new TicketInfoWindow();
+            infoWin.DataContext = ticket;
+            infoWin.Show();
         }
     }
 }
---
+++ GeneralEmployeeTerminal/Views/TicketInfoWindow.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace GeneralEmployeeTerminal.Views;
+
+public partial class TicketInfoWindow : Window
+{
+    public TicketInfoWindow()
+    {
+        InitializeComponent();
+    }
+}
